b66a3773628a697cfd5215ce8c05a14877a2074f
joselopez1987 <65475350+joselopez1987@users.noreply.github.com>
2022-02-25 11:07:53 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+
+let countEl = document.getElementById("count-el")
+let count = 0
+let saveEl = document.getElementById("save-el")
+
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+
+    let dash = count + " - "
+    saveEl.textContent += dash
+    countEl.textContent = 0
+    count = 0
+    
+    
+}
